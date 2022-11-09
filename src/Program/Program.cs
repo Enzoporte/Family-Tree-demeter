de9615e0f575aa401ea59c424901c3ba452ada19
Enzoporte <enzocantoni03@gmail.com>
2022-11-09 10:10:58 -0300

    comments

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,10 +9,12 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            // Visitors
             HijoMayorVisitor MayorVisitor = new HijoMayorVisitor();
             SumarEdadVisitor SumVisitor = new SumarEdadVisitor();
             NombreLargoVisitor NombreLargoVisitor = new NombreLargoVisitor(); 
 
+            // Nodos
             Node n1 = new Node("Enzo", 70);
             Node n2 = new Node("Enzuli", 50);
             Node n3 = new Node("Enzalada", 48);
@@ -22,7 +24,7 @@ namespace Program
             Node n7 = new Node("EnzoCantante", 18);
             List<Node> NodeList = new List<Node> {n1, n2, n3, n4, n5, n6, n7};
 
-            
+            // Agregar Hijos
             n1.AddChildren(n2);
             n1.AddChildren(n3);
 
@@ -32,12 +34,16 @@ namespace Program
             n3.AddChildren(n6);
             n3.AddChildren(n7);
 
+            // Recorrer Nodos Hijos y aplicar los Visitors
             foreach (var node in NodeList)
             {
                 node.Accept(SumVisitor);
                 node.Accept(MayorVisitor);
                 node.Accept(NombreLargoVisitor);
             }
+
+            // Imprimir en pantalla
+
             Console.WriteLine(SumVisitor.SumaTotal);
             Console.WriteLine(NombreLargoVisitor.NombreLargo.Nombre);
             
@@ -45,7 +51,6 @@ namespace Program
             {
                 Console.WriteLine(item.Nombre + ", Edad: " + item.Edad);           
             }
-            // visitar el árbol aquí
         }
     }
 }
